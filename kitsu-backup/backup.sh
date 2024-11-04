af6f3a002218c4d0b38ecfcdad52c13cd7a196f7
Ian Pinder <ian.m.pinder@gmail.com>
2024-11-04 07:11:43 +0000

    incremental rsync backup script added for preview files

--- kitsu-backup/backup.sh
+++ kitsu-backup/backup.sh
@@ -1,34 +1,21 @@
 #!/bin/sh
 echo "RUNING KITSU BACKUP PROCESSES..."
 
-# Define the container name dynamically using COMPOSE_PROJECT_NAME
-ZOU_APP_CONTAINER="${COMPOSE_PROJECT_NAME}_zou-app"
-
-# Step 1: Database dump in the dynamically named container
-echo "Starting database dump in the $ZOU_APP_CONTAINER container..."
-
-# Ensure the backup directory exists in the container
-docker exec "$ZOU_APP_CONTAINER" sh -c "mkdir -p /opt/zou/previews/db-backups"
-
-# Run the database dump command, setting the working directory
-docker exec "$ZOU_APP_CONTAINER" sh -c "cd /opt/zou/previews/db-backups && DB_PASSWORD=$DB_PASSWORD zou dump-database"
-
-echo "Database dump completed."
 
 # Step 2: Rsync backup process
 echo "Starting rsync backup..."
-if [ ! -d "$RSYNC_SRC" ]; then
-  echo "Source directory $RSYNC_SRC does not exist. Exiting."
-  exit 1
-fi
+# if [ ! -d "$RSYNC_SRC" ]; then
+#   echo "Source directory $RSYNC_SRC does not exist. Exiting."
+#   exit 1
+# fi
 
 # Define the destination directory for the backup
-DEST="${RSYNC_DEST}/${BACKUP_SUBFOLDER}"
+DEST="/backup-dest/${PREVIEWS_BACKUP_SUBFOLDER}"
 if [ ! -d "$DEST" ]; then
   echo "Creating backup destination $DEST."
   mkdir -p "$DEST"
 fi
 
 # Run the rsync command
-rsync -av --delete "$RSYNC_SRC/" "$DEST/"
+rsync -av --delete "/backup-source/" "$DEST/"
 echo "Backup completed."
