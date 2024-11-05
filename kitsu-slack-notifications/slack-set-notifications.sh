ea7df31f891cc05481364a2c218da10255afa9cc
Ian Pinder <ian.m.pinder@gmail.com>
2024-11-05 16:38:48 +0000

    add slack notifications bash script - temp

---
+++ kitsu-slack-notifications/slack-set-notifications.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Starting Slack notifications scheduled setup..."
+echo ""
+echo "Placholder function - not yet implemented
+
+
+
+
+
+# if [ $? -eq 0 ]; then
+#     echo "Previews backup completed successfully."
+# else
+#     echo "Previews backup encountered an error."
+# fi
+
+
