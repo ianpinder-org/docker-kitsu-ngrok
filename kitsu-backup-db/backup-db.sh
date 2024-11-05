4dc06429b2bfedb2229569aa56ae1cc40e2a1278
Ian Pinder <ian.m.pinder@gmail.com>
2024-11-05 07:26:44 +0000

    add timestamp string code

--- kitsu-backup-db/backup-db.sh
+++ kitsu-backup-db/backup-db.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-echo "RUNING KITSU DB BACKUP PROCESSES..."
+echo "RUNNING KITSU DB BACKUP PROCESSES..."
+
+# Get the current date and time in YYYYMMDD_HHMMSS format
+CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S")
 
 # Step 1: Database dump in the dynamically named container
 echo "Starting database dump..."
