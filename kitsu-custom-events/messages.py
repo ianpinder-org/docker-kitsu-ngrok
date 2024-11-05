f4fb3c0dfe0e31c992a3e27d6ed30dbb5c145182
Ian Pinder <ian.m.pinder@gmail.com>
2024-11-05 17:27:23 +0000

    added preview status update message

--- kitsu-custom-events/messages.py
+++ kitsu-custom-events/messages.py
@@ -7,12 +7,14 @@ def say(lang, phrase):
             'plan_short': 'Факт дольше плана (дни): ',
             'plan_long': 'Факт быстрее плана (дни)',
             'cant_skip': 'Невозможно изменить статус задачи, тк предыдущие задачи не закрыты'
+            'preview_update': '---'
         },
         'en': {
             'rtw_changed': 'Status changed to Ready to Work (RTW)',
             'plan_short': 'Actual duration took longer than planned (days): ',
             'plan_long': 'Actual duration took faster than planned (days): ',            
-            'cant_skip': 'Can\'t skip previous task(s). Status change aborted'
+            'cant_skip': 'Can\'t skip previous task(s). Status change aborted',
+            'preview_update': 'Status automatically updated for task with new preview added'
         }
     }
 
