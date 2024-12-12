3de6b40d34bf4b43cc44ec6a6cf103a5d292b0ea
Ian Pinder <ian.m.pinder@gmail.com>
2024-12-12 16:59:00 +0000

    wip temp added kitsu subdomain cert commands

--- copy_certs.sh
+++ copy_certs.sh
@@ -4,7 +4,11 @@
 mkdir -p /etc/nginx/certs/gwdoodles.com
 cp /etc/letsencrypt/live/gwdoodles.com/fullchain.pem /etc/nginx/certs/gwdoodles.com/
 cp /etc/letsencrypt/live/gwdoodles.com/privkey.pem /etc/nginx/certs/gwdoodles.com/
+# mkdir -p /etc/nginx/certs/kitsu.gwdoodles.com
+# cp /etc/letsencrypt/live/kitsu.gwdoodles.com/fullchain.pem /etc/nginx/certs/gwdoodles.com/
+# cp /etc/letsencrypt/live/kitsu.gwdoodles.com/privkey.pem /etc/nginx/certs/gwdoodles.com/
 ls -la /etc/nginx/certs/gwdoodles.com
+# ls -la /etc/nginx/certs/kitsu.gwdoodles.com
 chown -R nginx:nginx /etc/nginx/certs
 chmod -R 600 /etc/nginx/certs
 
